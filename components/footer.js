ab6c7fca5adde79fc177278963a13db184f6f0af
vaibhav admane <vaibhavadmane07@gmail.com>
2025-07-04 01:19:56 +0530

    convertion complete

--- components/footer.js
+++
@@ -1,9 +0,0 @@
-function createFooter() {
-  const footer = document.createElement('footer');
-  footer.style.padding = '2rem';
-  footer.style.backgroundColor = '#222';
-  footer.style.color = 'white';
-  footer.style.textAlign = 'center';
-  footer.textContent = "© 2025 Sample Project. All rights reserved.";
-  return footer;
-}
